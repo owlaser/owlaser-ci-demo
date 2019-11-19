0eb3c22caec00d19d6fba3cc1ea66da1d4bf268f
Solyn <solyn@pku.edu.cn>
2019-11-19 15:54:12 +0800

    Update TestDemo.java

--- src/test/java/TestDemo.java
+++ src/test/java/TestDemo.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class TestDemo {
-    private String message = "HelloWorld";
+    private String message = "Hello";
     private Demo demo = new Demo(this.message);
     @Test
     public void testPrintMessage() {
